99fe7c3bdc453019433fde3d6572b5ab508034d5
titkonen <toni.itkonen@icloud.com>
2020-05-11 08:53:46 +0300

    new url added > Boston

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,8 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'http://api.weatherstack.com/current?access_key=28bbcb97269871b7d9d1a59fa40814d9&query=boston'
+    // const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
     // const url = 'http://api.weatherstack.com/current?access_key=28bbcb97269871b7d9d1a59fa40814d9&query=' + latitude + '' + longitude + '&units=f'
 
 
